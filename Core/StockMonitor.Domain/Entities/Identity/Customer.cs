d9a2724d717c59c6fa5d43d913ee10d729a45768
mikail-torun@hotmail.com <mikail-torun@hotmail.com>
2023-01-05 02:22:25 +0300

    test2

--- Core/StockMonitor.Domain/Entities/Identity/Customer.cs
+++ Core/StockMonitor.Domain/Entities/Identity/Customer.cs
@@ -14,7 +14,6 @@ namespace StockMonitor.Domain.Entities.Identity
         public string Name{ get; set; }
         public string Email { get; set; }
         public string? Phone { get; set; }
-
         public string? Address { get; set; }
     }
 }
